983828380bb37b8ab9b92d6100b5bf2b22024ef1
TyoKuncoro <tyo.kuncoro2494@gmail.com>
2024-11-04 17:56:24 +0700

    search component

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        primaryColor: Color(0xFF1976D2),
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.blue,
+        ).copyWith(
+          primary: Color(0xFF1976D2),
+          secondary: Color(0xFFBBDEFB), // Use this instead of accentColor
+          tertiary: Color(0xFF0D47A1),
+        ),
+
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
@@ -30,10 +39,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Homepage'),
     );
   }
 }
@@ -62,19 +71,44 @@ class _MyHomePageState extends State<MyHomePage> {
         MaterialPageRoute(builder: (context) => const CameraPreviewWidget()));
   }
 
+  bool isSearching = false;
+
+  TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: !isSearching
+            ? Text("Image to Text")
+            : TextField(
+                controller: searchController,
+                autofocus: true,
+                decoration: InputDecoration(
+                  hintText: "Search...",
+                  border: InputBorder.none,
+                ),
+                style: TextStyle(color: Colors.white),
+              ),
+        actions: [
+          IconButton(
+            icon: Icon(isSearching ? Icons.close : Icons.search),
+            onPressed: () {
+              setState(() {
+                isSearching = !isSearching;
+                if (!isSearching) searchController.clear();
+              });
+            },
+          ),
+        ],
       ),
       body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             EmptyState(
-              message: "Tidak Ada Data",
+              message: "No data",
             ),
           ],
         ),
